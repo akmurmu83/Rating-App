8e744a3935a9f4a18bce825f9c4ab8fd938e3b72
Amit Kumar <amitkumar655921@gmail.com>
2023-11-29 21:04:59 +0530

    Day-03 integrated Chakra ui

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import Styles from './Navbar.module.css';
+import React from "react";
+import { Flex, Box, Link, Spacer, useTheme } from "@chakra-ui/react";
 
 function Navbar() {
+  const theme = useTheme(); // Access the Chakra UI theme
   return (
-    <nav className={Styles.nav}>
-      <div className={Styles.rightSection}></div>
-      <div className="logo">
-        <img src="" alt="" />
-      </div>
-      <div className="leftSection"></div>
-    </nav>
+    <Flex bg={theme.colors.primary[500]} p={4}>
+      <Box ml={4}>
+        <Link href="#" fontSize="xl" fontWeight="bold" color="white">
+          Rating App
+        </Link>
+      </Box>
+      <Spacer />
+      <Box>
+        <Link mr={5} href="#" fontSize="md" fontWeight="normal" color="white">
+          Your Portfolio
+        </Link>
+        <Link mr={5} href="#" fontSize="md" fontWeight="normal" color="white">
+          Your Portfolio
+        </Link>
+      </Box>
+    </Flex>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create or extend your Chakra UI theme
+const theme = extendTheme({
+  colors: {
+    primary: {
+      500: "teal", // Customize the primary color
+    },
+  },
+  fonts: {
+    body: "Roboto, sans-serif", // Customize the body font
+  },
+  // Additional theme configuration goes here
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ChakraProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
